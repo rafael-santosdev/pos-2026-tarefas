a6cc99f0cf9bc4b8fbd79fe497416da8cbc1dbca
Rafael Santos <rafaeldossantosdasilva13@gmail.com>
2026-06-12 23:59:11 -0300

    Ajuste

--- tarefa13/soap_zeep.py
+++ tarefa13/soap_zeep.py
@@ -1,13 +1,19 @@
 import zeep
 
-wsdl_url = "http://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL"
+print("Iniciando o programa...")
+
+wsdl_url = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL"
+
+print("Conectando ao serviço SOAP...")
 
 client = zeep.Client(wsdl=wsdl_url)
+
+print("Conexão realizada com sucesso.")
 
 number = input("Digite um número inteiro: ")
 
 result = client.service.NumberToWords(
-    ubiNum=number
+    ubiNum=int(number)
 )
 
 print("Número por extenso em inglês:")
